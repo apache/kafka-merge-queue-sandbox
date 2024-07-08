177a7d60e7d9cc00ce9bf36edfe16be0259e1043
David Arthur <mumrah@gmail.com>
2024-07-08 09:45:26 -0400

    change poison strings

--- pr-check.sh
+++ pr-check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 for FILE in `git diff-index --name-status origin/main -- | cut -c3-` ; do
-    if grep -q 'FAIL' "$FILE"
+    if grep -q 'FAILPR' "$FILE"
     then
-        echo $FILE ' contains FAIL!'
+        echo $FILE ' contains FAILPR!'
         exit 1
     fi
 done
