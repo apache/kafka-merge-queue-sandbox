2920e9d121134a5b5b2194950b0c11fa196a1f84
David Arthur <mumrah@gmail.com>
2024-07-08 09:46:20 -0400

    ISSUE-1000 Add separate script for merge queue (#8)
    * Add separate validation script for merge queue
    
    * change poison strings

--- queue-check.sh
+++ queue-check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 for FILE in `git diff-index --name-status origin/main -- | cut -c3-` ; do
-    if grep -q 'FAILPR' "$FILE"
+    if grep -q 'FAILQUEUE' "$FILE"
     then
-        echo $FILE ' contains FAILPR!'
+        echo $FILE ' contains FAILQUEUE!'
         exit 1
     fi
 done
