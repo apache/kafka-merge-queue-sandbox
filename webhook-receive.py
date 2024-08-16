29b76dcb90a355285f15c44aae7affaf75647898
David Arthur <mumrah@gmail.com>
2024-08-16 14:27:14 -0400

    WIP

--- webhook-receive.py
+++ webhook-receive.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook_receiver():
-    data = request.json  # Get the JSON data from the incoming request
-    # Process the data and perform actions based on the event
+    data = request.json
     print("Received webhook data:", data)
     return jsonify({'message': 'Webhook received successfully'}), 200
 
